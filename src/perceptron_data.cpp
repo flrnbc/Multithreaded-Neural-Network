bde55c252aa778fa5ced0041c5992397339980bc
Florian Beck <flrn.beck@gmail.com>
2021-10-25 10:56:17 +0200

    Small change

--- src/perceptron_data.cpp
+++ src/perceptron_data.cpp
@@ -57,7 +57,6 @@ std::vector<std::vector<double> > PerceptronData::WeightInitialization(int rows,
         int inputPlusOutputSize = cols + rows;
         // NOTE: uniform_real_distribution(a, b) generates for [a, b) (half-open interval)
         double min = -(sqrt(6)/sqrt(inputPlusOutputSize));
-        std::cout << "min: " << min << std::endl;
         double max = sqrt(6)/sqrt(inputPlusOutputSize);
 
         // randomly initialize weights

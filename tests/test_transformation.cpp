0f84b064b7db402daac9fe8cf4aee721322cf8c2
Florian Beck <flrn.beck@gmail.com>
2021-11-06 17:27:04 +0100

    Add tests for Transformation

---
+++ tests/test_transformation.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "../src/transformation.h"
+
+void test_RandomWeightInitialization() {
+    auto t = LinearTransformation(3, 3, "He");
+    auto t2 = LinearTransformation(3, 6, "Xavier");
+
+    std::cout << "Transformation t: " << std::endl;
+    std::cout << t.Summary() << std::endl;
+
+    std::cout << "Transformation t2: " << std::endl;
+    std::cout << t2.Summary() << std::endl;
+}
+
+void test_LinearTransform () {
+    auto t = LinearTransformation(3, 6, "He");
+    std::vector<double> e1{1.0, 0, 0, 0, 0, 0};
+    std::vector<double> e2{0, 1.0, 0, 0, 0, 0};
+
+    std::cout << "Transformation t: " << std::endl;
+    std::cout << t.Summary() << std::endl;
+
+    std::cout << "Transform e1: " << std::endl;
+    for (int i=0; i<3; i++) {
+        std::cout << t.Transform(e1)[i] << std::endl;
+    }
+
+    std::cout << "Transform e2: " << std::endl;
+    for (int i=0; i<3; i++) {
+        std::cout << t.Transform(e2)[i] << std::endl;
+    }
+}
+
+int main() {
+    test_RandomWeightInitialization();
+    test_LinearTransform();
+}

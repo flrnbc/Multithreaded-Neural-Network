2d60db0f16ae2054752d34514ebf5dcd3c78b93e
Florian Beck <flrn.beck@gmail.com>
2021-11-23 11:25:13 +0100

    Add more tests for transformations

--- tests/test_transformation.cpp
+++ tests/test_transformation.cpp
@@ -2,8 +2,10 @@
 #include "../src/transformation.h"
 
 void test_RandomWeightInitialization() {
-    auto t = LinearTransformation(3, 3, "He");
-    auto t2 = LinearTransformation(3, 6, "Xavier");
+    auto t = LinearTransformation(3, 3);
+    t.Initialize("He");
+    auto t2 = LinearTransformation(3, 6);
+    t2.Initialize("Xavier");
 
     std::cout << "Transformation t: " << std::endl;
     std::cout << t.Summary() << std::endl;
@@ -13,7 +15,8 @@ void test_RandomWeightInitialization() {
 }
 
 void test_LinearTransform () {
-    auto t = LinearTransformation(3, 6, "He");
+    auto t = LinearTransformation(3, 6);
+    t.Initialize("He");
     std::vector<double> e1{1.0, 0, 0, 0, 0, 0};
     std::vector<double> e2{0, 1.0, 0, 0, 0, 0};
 
@@ -31,7 +34,23 @@ void test_LinearTransform () {
     }
 }
 
+void test_ActivationTransform () {
+    auto a = ActivationTransformation(5, "relu");
+    std::vector<double> e1{1.0, 0, 0, 0, 0};
+    std::vector<double> e2{0, 1.0, 0, 0, 0};
+
+    std::cout << "Transformation a: " << std::endl;
+    std::cout << a.Summary() << std::endl;
+
+    std::cout << "Transform e2: " << std::endl;
+    for (int i=0; i<5; i++) {
+        std::cout << a.Transform(e2)[i] << std::endl;
+    }
+
+}
+
 int main() {
     test_RandomWeightInitialization();
     test_LinearTransform();
+    test_ActivationTransform();
 }

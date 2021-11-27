4cea824b2d093bfbd66b11cefcdd2fb2f9a88074
Florian Beck <flrn.beck@gmail.com>
2021-11-27 09:51:57 +0100

    AModify tests

--- tests/tests_sequential_nn.cpp
+++ tests/tests_sequential_nn.cpp
@@ -6,5 +6,13 @@
 #include <vector>
 
 void test_SequentialNN() {
-    SequentialNN snn({LinearLayer(20, 50), ActivationLayer(20, "relu")});
+    LinearLayer ll(20, 50);
+    ActivationLayer al(20, "relu"); 
+    std::vector<std::unique_ptr<Layer> > v{std::make_unique<Layer>(ll), std::make_unique<Layer>(al)};
+
+    SequentialNN snn(v);
+    std::vector<double> w(50, 1);
+
+    snn.Input(w);
+    snn.Forward();
 }

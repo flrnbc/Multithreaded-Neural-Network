5165d90bc82c474a962fe953a1718b0bdcb7cba9
Florian Beck <fbeck@Florians-MacBook-Air.local>
2021-12-23 10:36:32 +0100

    Adapting tests to Eigen.

--- tests/test_transformation.cpp
+++ tests/test_transformation.cpp
@@ -1,8 +1,9 @@
+#include <Eigen/Dense>
 #include <iostream>
 #include "../src/transformation.h"
 
 void test_RandomWeightInitialization() {
-    LinearTransformation t = LinearTransformation(3, 3);
+    auto t = LinearTransformation(3, 3);
     t.Initialize("He");
     auto t2 = LinearTransformation(3, 6);
     t2.Initialize("Xavier");
@@ -17,36 +18,29 @@ void test_RandomWeightInitialization() {
 void test_LinearTransform () {
     auto t = LinearTransformation(3, 6);
     t.Initialize("He");
-    std::vector<double> e1{1.0, 0, 0, 0, 0, 0};
-    std::vector<double> e2{0, 1.0, 0, 0, 0, 0};
+    Eigen::VectorXd e1{{1.0, 0, 0, 0, 0, 0}};
+    Eigen::VectorXd e2{{0, 1.0, 0, 0, 0, 0}};
 
     std::cout << "Transformation t: " << std::endl;
     std::cout << t.Summary() << std::endl;
 
     std::cout << "Transform e1: " << std::endl;
-    for (int i=0; i<3; i++) {
-        std::cout << t.Transform(e1)[i] << std::endl;
-    }
+    std::cout << t.Transform(e1) << std::endl;
 
     std::cout << "Transform e2: " << std::endl;
-    for (int i=0; i<3; i++) {
-        std::cout << t.Transform(e2)[i] << std::endl;
-    }
+    std::cout << t.Transform(e2) << std::endl;
 }
 
 void test_ActivationTransform () {
     auto a = ActivationTransformation(5, "relu");
-    std::vector<double> e1{1.0, 0, 0, 0, 0};
-    std::vector<double> e2{0, 1.0, 0, 0, 0};
+    Eigen::VectorXd e1{{1.0, 0, 0, 0, 0}};
+    Eigen::VectorXd e2{{0, 1.0, 0, 0, 0}};
 
     std::cout << "Transformation a: " << std::endl;
     std::cout << a.Summary() << std::endl;
 
     std::cout << "Transform e2: " << std::endl;
-    for (int i=0; i<5; i++) {
-        std::cout << a.Transform(e2)[i] << std::endl;
-    }
-
+    std::cout << a.Transform(e2) << std::endl;
 }
 
 int main() {

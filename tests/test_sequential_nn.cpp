9f8db46d880f57f589943c1e5a726cbc2e3edbc3
Florian Beck <fbeck@Florians-Air.fritz.box>
2022-01-01 18:22:17 +0100

    Add test for backward pass

--- tests/test_sequential_nn.cpp
+++ tests/test_sequential_nn.cpp
@@ -33,7 +33,7 @@ void test_ConnectLayers() {
     std::cout << "Output of al_ptr: " << al_ptr->Output() << std::endl;
 }
 
-void test_SequentialNN() {
+void test_SequentialNNForward() {
     auto ll_ptr = std::make_shared<LinearLayer>(2, 5);
     auto al_ptr = std::make_shared<ActivationLayer>(2, "relu"); 
     //std::vector<Layer> v{std::move(ll), std::move(al)};
@@ -52,9 +52,30 @@ void test_SequentialNN() {
     std::cout << "Output: \n" << snn.Output() << std::endl;
 }
 
+void test_SequentialNNBackward() {
+    auto ll_ptr = std::make_shared<LinearLayer>(2, 5);
+    auto al_ptr = std::make_shared<ActivationLayer>(2, "softmax"); 
+    //std::vector<Layer> v{std::move(ll), std::move(al)};
+
+    SequentialNN snn({ll_ptr, al_ptr});
+    std::cout << snn.Summary() << std::endl;
+
+    Eigen::VectorXd w{{1, 2, 3, 4, 5}};
+    Eigen::RowVectorXd gradL{{5, 6}}; // typically gradient of loss function
+
+    snn.Initialize();
+    std::cout << snn.Summary() << std::endl;
+
+    snn.BackwardInput(w);
+    snn.Backward();
+
+    std::cout << "Output: \n" << snn.BackwardOutput() << std::endl;
+}
+
 int main() {
     //test_ConnectLayers();
-    test_SequentialNN();
+    // test_SequentialNNForward();
+    test_SequentialNNBackward();
     //test_GetInitializationType();
 
     return 0;

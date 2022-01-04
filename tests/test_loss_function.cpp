3ec1979c7632f3aae4e5886e385ffc7f20563b72
Florian Beck <fbeck@florians-air.speedport.ip>
2022-01-04 18:30:28 +0100

    Simple test for MSE

---
+++ tests/test_loss_function.cpp
@@ -0,0 +1,19 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include "../src/loss_function.h"
+
+void test_MSE() {
+    auto mse = MSE(5);
+    Eigen::VectorXd y{{1, 2, 2, 1, 0}};
+    Eigen::VectorXd yLabel{{1, 1, 0, 1, -1}};
+
+    std::cout << yLabel.size() << std::endl;
+
+    std::cout << mse.ComputeLoss(y, yLabel) << std::endl;
+    mse.UpdateGradient(y, yLabel);
+    std::cout << mse.Gradient() << std::endl;
+}
+
+int main() {
+    test_MSE();
+}
